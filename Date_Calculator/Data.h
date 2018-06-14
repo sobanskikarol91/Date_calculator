4d07e596ab8e44a86799829933c709088213466a
DESKTOP-FALGG2T\Karol Sobanski <sobanskikarol91@gmail.com>
2018-06-14 15:19:11 +0200

    Calculator Completed

--- Date_Calculator/Data.h
+++ Date_Calculator/Data.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 #define ILOSC_MIESIECY 12
@@ -12,51 +13,28 @@ class Data
 	int dzien;
 	int miesiac;
 	int rok;
-	int * dni_miesiaca;
+	int dni_miesiaca[ILOSC_MIESIECY] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 public:
 	Data() : Data(0, 0, 0) {}
 	Data(int dzien) : Data(dzien, 0, 0) {}
-	Data(int dzien, int miesiac, int rok) : dzien(dzien), miesiac(miesiac), rok(rok)
-	{
-		utworz_miesiace();
-		try
-		{
-			//sprawdzenie_daty();
-		}
-		catch (int blad)
-		{
-			if (blad == 0) cerr << "Niepoprawny miesiac i dzien" << endl;
-			else if (blad == 1) cerr << "Niepoprawny miesiac" << endl;
-			else if (blad == 2) cerr << "Niepoprawny dzien" << endl;
 
-			*this = Data(); // inicjalizujemy wszystko zerami.
-		}
-	}
+	Data(string data) : Data(stoi(data.substr(0, 2)), stoi(data.substr(3, 2)), stoi(data.substr(6, 4))) {}
 
-	~Data()
-	{
-		// intuwamy dynamicznie przydzielona pamiec
-		delete[] dni_miesiaca;
-	}
+	Data(int dzien, int miesiac, int rok) : dzien(dzien), miesiac(miesiac), rok(rok) {}
 
 	Data operator+(int dodaj_dni)
 	{
 		int suma_dni_do_dodania = oblicz_dni_od_poczatku_roku() + dodaj_dni;
 		int obliczony_rok = oblicz_lata_po_dodaniu_dni(suma_dni_do_dodania, rok);
-		cout << "dni od poczatku:" << suma_dni_do_dodania << endl;
 		int obliczony_miesiac = oblicz_ile_miesiecy(suma_dni_do_dodania, obliczony_rok);
-		cout << "dni od poczatku:" << suma_dni_do_dodania << endl;
-
 		return Data(suma_dni_do_dodania, obliczony_miesiac, obliczony_rok);
 	}
 
 	Data operator+(Data data)
 	{
 		int dni_do_dodania = oblicz_dni_od_poczatku_roku() + data.oblicz_dni_od_poczatku_roku();
-		cout << "Dni od poczatku: " << dni_do_dodania << endl;
-
 		int obliczony_rok = oblicz_lata_po_dodaniu_dni(dni_do_dodania, rok + data.rok);
-		cout << "Dni na miesiace: " << dni_do_dodania << endl;
 		int obliczony_miesiac = oblicz_ile_miesiecy(dni_do_dodania, obliczony_rok);
 
 		return  Data(dni_do_dodania, obliczony_miesiac, obliczony_rok);
@@ -65,27 +43,45 @@ public:
 	Data operator-(int dni_ktore_odejmujemy)
 	{
 		int roznica_dni = oblicz_dni_od_poczatku_roku() - dni_ktore_odejmujemy;
-		int obliczony_rok=rok, obliczony_miesiac = 0;
-		cout << "Roznica dni: " << roznica_dni << endl;
+		int obliczony_rok = rok, obliczony_miesiac = 0;
 
 		if (roznica_dni < 0)
 		{
 			roznica_dni *= (-1);
 			obliczony_rok = oblicz_lata_po_odjeciu_dni(roznica_dni, rok);
-			cout << "Roznica lat:" << obliczony_rok << endl;
 		}
-		if(roznica_dni > 0)
-		{
-			 obliczony_miesiac = oblicz_ile_miesiecy(roznica_dni, obliczony_rok);
-		}
+		if (roznica_dni > 0)
+			obliczony_miesiac = oblicz_ile_miesiecy(roznica_dni, obliczony_rok);
 
 		return Data(roznica_dni, obliczony_miesiac, obliczony_rok);
 	}
 
+	Data operator-(Data data)
+	{
+		int dni = zamien_date_na_dni(data);
+		return *this - dni;
+	}
+
+	int zamien_date_na_dni(Data data)
+	{
+		int dni = 0;
+
+		// lata na dni
+		for (size_t i = 1; i < data.rok; i++)
+			dni += ile_dni_ma_rok(i);
+
+		// miesiace na dni
+		for (size_t i = 0; i < data.miesiac; i++)
+			dni += oblicz_ile_dni_w_miesiacu(data.rok, i);
+
+		// dni
+		dni += data.dzien;
+		return dni;
+	}
 
 	friend ostream & operator<<(ostream & ekran, const  Data & d)
 	{
-		return ekran << d.dzien << "-" << d.miesiac << "-" << d.rok << endl;
+		return ekran << d.dzien << "-" << d.miesiac << "-" << d.rok;
 	}
 
 	bool czy_przestepny(int rok)
@@ -93,27 +89,7 @@ public:
 		return ((rok % 4 == 0) && (rok % 100 != 0)) || (rok % 400 == 0);
 	}
 
-	void sprawdzenie_daty()
-	{
-		//cout << "miesiac" << miesiac << endl;
-		bool miesiace_blad, dni_blad;
-		miesiace_blad = miesiac > ILOSC_MIESIECY;
-		dni_blad = dzien > dni_miesiaca[miesiac - 1];
-
-
-		if (miesiace_blad && dni_blad) throw 0;
-		else if (miesiace_blad) throw 1;
-		else if (dni_blad) throw 2;
-	}
-
 public:
-	void utworz_miesiace()
-	{
-		short luty = czy_przestepny(rok) ? 29 : 28;
-		// tworzymy dynamicznie aby to od razu zainicjalizowac w 1 linijce
-		dni_miesiaca = new int[ILOSC_MIESIECY] { 31, luty, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	}
-
 
 	int oblicz_lata_po_dodaniu_dni(int & dni_do_dodania, int licz_od_roku)
 	{
@@ -122,9 +98,7 @@ public:
 		// jezeli dni jest wiecej niz w danym roku 
 		while (dni_do_dodania > (dni_w_roku = ile_dni_ma_rok(licz_od_roku)))
 		{
-			cout << "rok: " << licz_od_roku << "  " << ile_dni_ma_rok(licz_od_roku) << endl;
 			dni_do_dodania -= dni_w_roku;
-			cout << dni_do_dodania << endl;
 			licz_od_roku++;
 		}
 		return licz_od_roku;
@@ -134,20 +108,16 @@ public:
 	{
 		int dni_w_roku;
 
-		cout << "ile dni przed odejmowaniem " << pozostale_dni << endl;
 
 		while (pozostale_dni > 0)
 		{
 			dni_w_roku = ile_dni_ma_rok(licz_od_roku);
-			cout << "Dni w roku: " << dni_w_roku << "  Odejmij" << pozostale_dni << endl;
-
 			dni_w_roku -= pozostale_dni;
 			pozostale_dni -= ile_dni_ma_rok(licz_od_roku);
 			licz_od_roku--;
 		}
 
 		pozostale_dni = dni_w_roku;
-		cout << "dni po udejmownaiu: " << dni_w_roku << endl;
 		return licz_od_roku;
 	}
 
@@ -162,7 +132,6 @@ public:
 		for (size_t i = 0; i < miesiac - 1; i++)
 			dni_roku += oblicz_ile_dni_w_miesiacu(rok, i);
 
-		cout << "dni od poczatku:" << dni_roku << endl;
 		return dni_roku;
 	}
 
@@ -170,14 +139,9 @@ public:
 	{
 		short dni_w_miesiacu = 0, nr_miesiac = 1;
 
-		cout << "Oblicz miesiace: " << pozostale_dni << endl;
-		// jezeli dni jest wiecej niz w danym roku 
-
-		while (pozostale_dni > (dni_w_miesiacu = oblicz_ile_dni_w_miesiacu(obliczony_rok, nr_miesiac - 1)) )
+		while (pozostale_dni > (dni_w_miesiacu = oblicz_ile_dni_w_miesiacu(obliczony_rok, nr_miesiac - 1)))
 		{
-			cout << "Od Dni: " << pozostale_dni << " Odejmnij: " << dni_w_miesiacu << endl;
 			pozostale_dni -= dni_w_miesiacu;
-			cout << pozostale_dni << endl;
 			nr_miesiac++;
 		}
 		// zwracamy ile jest pelnych miesiecy
@@ -197,4 +161,3 @@ public:
 		else return dni_miesiaca[miesiac];
 	}
 };
-
--- Date_Calculator/Source.cpp
+++ Date_Calculator/Source.cpp
@@ -2,17 +2,33 @@
 #include "Data.h"
 using namespace std;
 
-int main()
-{
-	//Data d1(12,06,2018), d2(20,04,20);
-	//Data d1(1, 1, 2018), d2(31, 12, 20);
-	Data d1(20, 11, 2018), d2(31, 12, 20);
-	//Data d3 = d1  - 689;
-	Data d3 = d1 + d2;
-	cout << d1;
-	//cout << d2;
+#include <string>
 
-	cout << d3;
+
+void linux(int argc, char* argv[])
+{
+	if (*argv[2] == '+')
+		cout << Data(argv[1]) << " + " << Data(argv[3]) << " = " << Data(argv[1]) + Data(argv[3]) << endl;
+	else if (*argv[2] == '-')
+		cout << Data(argv[1]) << " - " << Data(argv[3]) << " = " << Data(argv[1]) - Data(argv[3]) << endl;
+
 	getchar();
+}
+
+
+int main(int argc, char* argv[])
+{
+	char sciezka[] = "./plik";
+	char data1[] = "10-03-0105";
+	char znak[1]{ '+' };
+	char data2[] = "10-07-1205";
+
+	const int rozmiar = 4;
+	char * wskaznik[rozmiar];
+	wskaznik[0] = sciezka;
+	wskaznik[1] = data1;
+	wskaznik[2] = znak;
+	wskaznik[3] = data2;
+	linux(rozmiar, wskaznik);
 	return 0;
 }
